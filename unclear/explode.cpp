401b870d2d375480fd86ac2f0b15d58cce4f4bee
Sool Uh! <suluh.webdevelopers@hotmail.com>
2021-11-14 13:18:13 +0700

    :scissors: explode function to split string

---
+++ unclear/explode.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <utility>
+
+using namespace std;
+
+vector<string> explode(string const &s, char delim)
+{
+	vector<string> result;
+	istringstream iss(s);
+
+	for (string token; getline(iss, token, delim);)
+	{
+		result.push_back(move(token));
+	}
+
+	return result;
+}
+
+int main()
+{
+	string hours;
+	string minutes;
+	string seconds;
+
+	string format = "hh:mm:ss";
+	char delimiter = ':';
+
+	cout << "Format : " << format << endl;
+
+	hours = explode(format, delimiter)[0];
+	minutes = explode(format, delimiter)[1];
+	seconds = explode(format, delimiter)[2];
+
+	cout << "Hours : " << hours << endl;
+	cout << "Minutes : " << minutes << endl;
+	cout << "Seconds : " << seconds << endl;
+
+	return 0;
+}
