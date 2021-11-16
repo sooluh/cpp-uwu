91c2deaa1f06de451092a6f84257ea2a8b9f075a
Sool Uh! <suluh.webdevelopers@hotmail.com>
2021-11-17 00:18:59 +0700

    :knife: add mid-semester test answers

---
+++ exam/20211.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	typedef struct
+	{
+		int nim;
+		string nama;
+		string prodi;
+	} Mahasiswa;
+	Mahasiswa mahasiswa;
+
+	typedef struct
+	{
+		long pembangunan;
+		long cicilan;
+		long kemahasiswaan;
+	} Biaya;
+	Biaya biaya;
+
+	long total;
+
+	cout << "TOTAL BIAYA MASUK KULIAH" << endl;
+	cout << "===========================" << endl;
+
+	cout << "Nama  : Suluh Sulistiawan" << endl;
+	cout << "Kelas : IF Malam B" << endl;
+	cout << "===========================" << endl;
+
+	cout << "Masukkan NIM Anda : ";
+	cin >> mahasiswa.nim;
+
+	cout << "Masukkan Nama Anda : ";
+	cin >> mahasiswa.nama;
+
+	cout << "Masukkan Prodi Anda : ";
+	cin >> mahasiswa.prodi;
+
+	cout << "Uang Pembangunan : ";
+	cin >> biaya.pembangunan;
+
+	cout << "Cicilan Pertama : ";
+	cin >> biaya.cicilan;
+
+	cout << "Uang Kemahasiswaan : ";
+	cin >> biaya.kemahasiswaan;
+
+	cout << "===========================" << endl;
+	total = biaya.pembangunan + biaya.cicilan + biaya.kemahasiswaan;
+
+	cout << "Halo " << mahasiswa.nama << " dengan NIM " << mahasiswa.nim << endl;
+	cout << "Total biaya kuliah kamu sekitar : " << total;
+
+	return 0;
+}
