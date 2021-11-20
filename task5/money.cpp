2b4296c0f9a4173b0eceaf1fbf7509e2b0c761cd
Sool Uh! <suluh.webdevelopers@hotmail.com>
2021-11-20 13:26:04 +0700

    :dart: money & midpoint

---
+++ task5/money.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	const int THOUSAND = 1000;
+	const int HUNDRED = 100;
+	const int FIFTY = 50;
+	const int TWENTY5 = 25;
+
+	bool run = true;
+	bool failed = false;
+	long int money, thousands, hundreds, fifties, twenty5;
+
+	while (run)
+	{
+		run = false;
+
+		if (failed == true)
+		{
+			cout << "Harap masukkan uang dengan kelipatan 25"
+				 << "\n";
+		}
+
+		cout << "Masukkan jumlah uang : ";
+		cin >> money;
+
+		if (money % 25 == 0)
+		{
+			thousands = money / THOUSAND;
+			money = money % THOUSAND;
+			hundreds = money / HUNDRED;
+			money = money % HUNDRED;
+			fifties = money / FIFTY;
+			money = money % FIFTY;
+			twenty5 = money / TWENTY5;
+
+			cout << "Pecahan 1000 : " << thousands << "\n";
+			cout << "Pecahan 100 : " << hundreds << "\n";
+			cout << "Pecahan 50 : " << fifties << "\n";
+			cout << "Pecahan 25 : " << twenty5 << "\n";
+		}
+		else
+		{
+			run = true;
+			failed = true;
+		}
+	}
+
+	system("pause");
+	return 0;
+}
---
+++ week7/midpoint.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	typedef struct
+	{
+		float x;
+		float y;
+	} Coordinate;
+	Coordinate p1, p2, p3;
+
+	cout << "Masukkan x1 : ";
+	cin >> p1.x;
+
+	cout << "Masukkan y1 : ";
+	cin >> p1.y;
+
+	cout << "Masukkan x2 : ";
+	cin >> p2.x;
+
+	cout << "Masukkan y2 : ";
+	cin >> p2.y;
+
+	p3.x = (p1.x + p2.x) / 2;
+	p3.y = (p1.y + p2.y) / 2;
+
+	cout << "P3 (" << p3.x << ", " << p3.y << ")";
+
+	cout << "\n";
+	system("pause");
+	return 0;
+}
